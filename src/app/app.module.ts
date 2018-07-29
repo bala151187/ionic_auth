0c704c264fb802ffdaffb93c9894425c43fdd364
bala151187 <balamurugan151187@gmail.com>
2018-07-29 17:23:10 -0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { ListPage } from '../pages/list/list';
+import { LoginPage } from '../pages/login/login';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthProvider } from '../providers/auth/auth';
+
+// Import Firebase / environment config and initialise
+import * as firebase from 'firebase';
+import { environment } from '../environments/environment';
+
+firebase.initializeApp(environment.firebase);
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    ListPage,
+    LoginPage
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    IonicModule.forRoot(MyApp),
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    ListPage,
+    LoginPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthProvider
+  ]
+})
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+   production: false,
+   firebase : {
+    apiKey: "AIzaSyChRobefGAl0bBjLIuQhveKK-NOjDPNFt0",
+    authDomain: "auth-dae37.firebaseapp.com",
+    databaseURL: "https://auth-dae37.firebaseio.com",
+    projectId: "auth-dae37",
+    storageBucket: "auth-dae37.appspot.com",
+    messagingSenderId: "480757765415" 
+   }
+   };
